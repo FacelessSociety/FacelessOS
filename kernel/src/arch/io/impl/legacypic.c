3920c0286ff994bdc349dc48d85b2fed91a66c89
Ian-M-Moffett <spruiousvector@gmail.com>
2022-05-15 17:38:19 -0400

    Added legacy PIC chipset driver.

---
+++ kernel/src/arch/io/impl/legacypic.c
@@ -0,0 +1,39 @@
+#include <arch/io/legacypic.h>
+#include <arch/io/io.h>
+
+// 2022 Ian Moffett <ian@kesscoin.com>
+
+
+void init_pic() {
+  outportb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);      // Init master PIC.
+  io_wait();
+  outportb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);      // Init slave PIC.
+  io_wait();
+
+  outportb(PIC1_DATA, 0x20);
+  io_wait();
+  outportb(PIC2_DATA, 0x28);
+  io_wait();
+
+  outportb(PIC1_DATA, 4);
+  io_wait();
+  outportb(PIC2_DATA, 2);
+  io_wait();
+
+  outportb(PIC1_DATA, ICW4_8086);
+  io_wait();
+  outportb(PIC2_DATA, ICW4_8086);
+  io_wait();
+
+  outportb(PIC1_DATA, 0xFF);
+  io_wait();
+  outportb(PIC2_DATA, 0xFF);
+}
+
+
+void PIC_sendEOI(unsigned char irq) {
+  if(irq >= 8)
+    outportb(PIC2_COMMAND,PIC_EOI);
+
+  outportb(PIC1_COMMAND,PIC_EOI);
+}
