28ba5a92c8c5c8573ae1aa4d90c35a91e606d201
Ian-M-Moffett <spruiousvector@gmail.com>
2022-05-17 00:32:01 -0400

    Updated libstring.

--- userspace/lib/libstring/impl/string.c
+++ userspace/lib/libstring/impl/string.c
@@ -6,3 +6,26 @@ size_t _strlen(const char* const STR) {
     while (STR[n++]);
     return n;
 }
+
+uint8_t _strncmp(char* s1, char* s2, uint32_t n) {
+    uint32_t n_cnt = 0;
+
+    while (s1 && s2 && n_cnt < n) {
+        if (*s1++ != *s2++) {
+            return 0;
+        }
+
+        ++n_cnt;
+    }
+
+    return 1;
+}
+
+
+void _memzero(void* ptr, size_t n) {
+    char* ptr_char = (char*)ptr;
+
+    for (size_t i = 0; i < n; ++i) {
+        ptr_char[i] = 0;
+    }
+}
--- userspace/lib/libstring/string.h
+++ userspace/lib/libstring/string.h
@@ -4,9 +4,11 @@
 // 2022 Ian Moffett <ian@kesscoin.com>
 
 #include <stddef.h>
+#include <stdint.h>
 
 size_t _strlen(const char* const STR);
-
+uint8_t _strncmp(char* s1, char* s2, uint32_t n);
+void _memzero(void* ptr, size_t n);
 
 
 #endif
