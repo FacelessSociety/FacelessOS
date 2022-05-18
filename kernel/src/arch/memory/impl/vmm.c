087fbc91b093f484975f4f79a3c1622d4a270878
Ian-M-Moffett <spruiousvector@gmail.com>
2022-05-18 16:21:23 -0400

    Fixed values.

--- kernel/src/arch/memory/impl/vmm.c
+++ kernel/src/arch/memory/impl/vmm.c
@@ -8,8 +8,8 @@
 
 #define PAGE_ADDR_MASK 0x000ffffffffff000
 #define GB 0x40000000UL
-#define KERNEL_RESERVED_START 0x0000000000005000
-#define KERNEL_RESERVED_END 0x8D84
+#define KERNEL_RESERVED_START 0x0000000000006000
+#define KERNEL_RESERVED_END 0x9D94
 
 
 __attribute__((aligned(0x1000))) static struct MappingTable {
