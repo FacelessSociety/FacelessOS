995b37ca33b77aecafce348b391fea3575a9a0b5
Ian-M-Moffett <spruiousvector@gmail.com>
2022-05-20 16:42:20 -0400

    Added keyboard helper.

---
+++ userspace/lib/libkeyboard/scancodes.h
@@ -0,0 +1,16 @@
+#ifndef LIBKEYBOARD_SCANCODES_H
+#define LIBKEYBOARD_SCANCODES_H
+
+// 2022 Ian Moffett <ian@kesscoin.com>
+
+
+typedef enum {
+    SC_UP = 0x48,
+    SC_DOWN = 0x50,
+    SC_LEFT = 0x4B,
+    SC_RIGHT = 0x4D,
+    SC_ENTER = 28
+} SCANCODES;
+
+
+#endif
